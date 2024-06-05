ed4fef3edf3060d8e306b79b98d4165e1633d7eb
decatoriar <rdecatoriajunior@gmail.com>
2024-06-05 15:25:16 +0800

    Add project files.

---
+++ MyDIAPI/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDIAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SBO.Connection connection = new SBO.Connection();
+            connection.Connect();
+
+            long returnCode;
+            string errMsg = string.Empty;
+
+            try
+            {
+                SAPbobsCOM.BusinessPartners businessPartner = (SAPbobsCOM.BusinessPartners)connection.company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oBusinessPartners);
+                businessPartner.CardCode = "C06524";
+                businessPartner.CardName = "Test Customer";
+                businessPartner.CardType = SAPbobsCOM.BoCardTypes.cCustomer;
+
+                businessPartner.CommissionGroupCode = 0;
+                businessPartner.CommissionPercent = 15;
+                businessPartner.CompanyPrivate = SAPbobsCOM.BoCardCompanyTypes.cPrivate;
+                businessPartner.ContactPerson = "C1";
+                businessPartner.Currency = "AUD";
+                businessPartner.DiscountPercent = 15;
+                businessPartner.VatLiable = SAPbobsCOM.BoVatStatus.vLiable;
+                businessPartner.ShippingType = 3;
+
+                businessPartner.ContactEmployees.Add();
+                businessPartner.ContactEmployees.SetCurrentLine(0);
+                businessPartner.ContactEmployees.Name = "C1";
+                businessPartner.ContactEmployees.Address = "LN";
+                businessPartner.ContactEmployees.E_Mail = "c1@abcd.com";
+                businessPartner.ContactEmployees.Fax = "8433777778";
+                businessPartner.ContactEmployees.MobilePhone = "8388888";
+                businessPartner.ContactEmployees.Phone1 = "88880000";
+
+                businessPartner.ContactEmployees.SetCurrentLine(1);
+                businessPartner.ContactEmployees.Name = "C2";
+                businessPartner.ContactEmployees.Address = "BJ";
+                businessPartner.ContactEmployees.E_Mail = "c2@abcd.com";
+                businessPartner.ContactEmployees.Fax = "84338";
+                businessPartner.ContactEmployees.MobilePhone = "877388888";
+                businessPartner.ContactEmployees.Phone1 = "8888300";
+
+                returnCode = businessPartner.Add();
+                if (returnCode == 0)
+                {
+                    connection.company.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_Commit);
+                }
+                else
+                {
+                    if (connection.company.InTransaction)
+                    {
+                        connection.company.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack);
+                    }
+
+                    errMsg = connection.company.GetLastErrorDescription();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ MyDIAPI/SBO/Connection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDIAPI.SBO
+{
+    class Connection
+    {
+        public SAPbobsCOM.Company company;
+
+        public SAPbobsCOM.BoDataServerTypes SQLVersion(string sqlVersion)
+        {
+            if (sqlVersion == "2016")
+            {
+                return SAPbobsCOM.BoDataServerTypes.dst_MSSQL2016;
+            }
+            else if (sqlVersion == "2017")
+            {
+                return SAPbobsCOM.BoDataServerTypes.dst_MSSQL2017;
+            }
+            if (sqlVersion == "2019")
+            {
+                return SAPbobsCOM.BoDataServerTypes.dst_MSSQL2019;
+            }
+
+            return SAPbobsCOM.BoDataServerTypes.dst_MSSQL2016;
+        }
+
+        public void Connect()
+        {
+            int lretcode = 0;
+            string errmsg = string.Empty;
+
+            if (company == null)
+            {
+                company = new SAPbobsCOM.Company();
+            }
+
+            if (company.Connected)
+            {
+                company.Disconnect();
+            }
+
+            try
+            {
+                company.Server = ConfigurationManager.AppSettings.GetValues("Server")[0];
+                company.CompanyDB = ConfigurationManager.AppSettings.GetValues("CompanyDB")[0];
+                company.UserName = ConfigurationManager.AppSettings.GetValues("UserName")[0];
+                company.Password = ConfigurationManager.AppSettings.GetValues("Password")[0];
+                company.DbUserName = ConfigurationManager.AppSettings.GetValues("DbUserName")[0];
+                company.DbPassword = ConfigurationManager.AppSettings.GetValues("DBPassword")[0];
+                company.DbServerType = SQLVersion(ConfigurationManager.AppSettings.GetValues("SQLVersion")[0]);
+                company.language = SAPbobsCOM.BoSuppLangs.ln_English;
+
+                lretcode = company.Connect();
+
+                if (lretcode != 0)
+                {
+                    errmsg = company.GetLastErrorDescription();
+                    Console.WriteLine(errmsg);
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
